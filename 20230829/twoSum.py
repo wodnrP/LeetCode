0ff966d6f84f94296d844302ad022d1f643ba0cd
wodnrP <wogajang22@naver.com>
2023-08-30 17:06:20 +0900

    contains Duplicate2 해결

--- 20230829/twoSum.py
+++ 20230829/twoSum.py
@@ -13,3 +13,19 @@ nums=[2,7,11,15]
 target=9
 print(Solution().twoSum(nums,target))
 """
+
+# 수정 - 타임아웃
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#             l=0
+#             r = 1
+#             while l<len(nums)-1:
+                
+#                 if r > len(nums)-1:
+#                     l+=1
+#                     r = l+1
+
+#                 if nums[l] + nums[r] == target:
+#                     return [l, r]
+                
+#                 r+=1
