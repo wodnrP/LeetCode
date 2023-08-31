20cb1b7a3c4af024d9836ae07240dd2c147080a6
wodnrP <wogajang22@naver.com>
2023-08-31 23:13:29 +0900

    happy number 코드 개선

--- 20230831/happyNumber.py
+++ 20230831/happyNumber.py
@@ -1,19 +1,41 @@
+# 1차
+# class Solution:
+#     def isHappy(self, n: int) -> bool:
+#         k = str(n)
+#         r = 0
+        
+#         while k != '1':
+#             if k == '1' or k =='7':
+#                 return True
+            
+#             elif int(k) < 10:
+#                 return False
+
+#             for i in range(len(k)):
+#                 r += int(k[i])**2
+            
+#             k = str(r)
+#             r = 0
+
+#             continue
+#         return True
+
+# 변수 개선
 class Solution:
-    def isHappy(self, n: int) -> bool:
-        k = str(n)
+    def isHappy(self, n: int) -> bool: 
         r = 0
         
-        while k != '1':
-            if k == '1' or k =='7':
+        while n != 1:
+            if n == 7:
                 return True
             
-            elif int(k) < 10:
+            elif n < 10:
                 return False
-
-            for i in range(len(k)):
-                r += int(k[i])**2
             
-            k = str(r)
+            for i in range(len(str(n))):
+                r += int(str(n)[i])**2
+            
+            n = r
             r = 0
 
             continue
