85877eba4e43d5e228eb2050626e7aae36740ead
wodnrP <wogajang22@naver.com>
2023-09-02 00:58:35 +0900

    hashTable 코드 수정

--- 20230901/hashTable.py
+++ 20230901/hashTable.py
@@ -9,17 +9,24 @@ class LinearProbingHash():
         hashKey = 0
 
         for i in key:
-            hashKey += ord(i) - ord('a')
-        hashKey %= self.tableSize
+            hashKey += ord(i) + ord('a')
 
+        hashKey %= self.tableSize
         return hashKey
+    
+    # key가 있는지 확인
+    def key_check(self, hashTable, key):
+            for i, table in enumerate(hashTable):
+                if key in table:
+                    return True
+            return False
     
     # hash key에 해당하는 주소에 key : value 쌍 저장
     def add_hash(self, key, value):
         hashKey = self.get_hash_key(key)
 
-        if self.hashTable[hashKey] == None:
-            self.hashTable[hashKey] = {key, value}
+        if not self.hashTable[hashKey]:
+            self.hashTable[hashKey] = {key : value}
             print(
                 "hashKey 주소: ", hashKey, 
                 "위치에 key:", key, 
@@ -30,15 +37,15 @@ class LinearProbingHash():
             _next = (hashKey + 1) % self.tableSize
 
             while self.hashTable[_next]:
-                _next = (hashKey + 1) % self.tableSize
+                _next = (_next + 1) % self.tableSize
 
                 if _next == hashKey:
                     print("빈 테이블이 없습니다.")
-                    return 
-                
-            self.hashTable[_next] = {key, value}
+                    return print("**********저장 불가**********\n")
+
+            self.hashTable[_next] = {key : value}
             print(
-                "hashKey 주소: ", hashKey, 
+                "hashKey 주소: ", _next, 
                 "위치에 key:", key, 
                 ",Value:", value, "이 저장 되었습니다."
                 )
@@ -47,26 +54,34 @@ class LinearProbingHash():
     def get_value(self, key):
         hashKey = self.get_hash_key(key)
 
-        if key in self.hashTable:
-            return self.hashTable[hashKey]
+        if self.key_check(self.hashTable, key):
+            for i in self.hashTable:
+                if key in i:
+                    # i.values = dict([값])이 나와서 
+                    # print문 깔끔히 하기 위해 임의 사용
+                    return next(iter(i.values()))
         
         else:
             _next = (hashKey + 1) % self.tableSize
-            
-            while key not in self.hashTable:
-                _next = (hashKey+1) % self.tableSize
+
+            while not self.key_check(self.hashTable, key):
+                _next = (_next+1) % self.tableSize
 
                 if _next == hashKey: 
-                    print("해당 ",key,"의 값을 찾지 못했습니다.")
-                    return 
-                
+                    print("해당 key",key,"의 값을 찾지 못했습니다.")
+                    return "없습니다."
+            
             return self.hashTable[_next]
         
 
-myHashTable = LinearProbingHash(10)
-myHashTable.add_hash("abc",10)
-myHashTable.add_hash("cba",20)
-print("key 값 abcd의 value는 ",myHashTable.get_value("abcd"))
-print("key 값 cbaf의 value는 ",myHashTable.get_value("cbaf"))
+# 동작 확인
+testHashTable = LinearProbingHash(2)
+testHashTable.add_hash("continued",10)
+testHashTable.add_hash("success",20)
+testHashTable.add_hash("unnoticed",30)
+
+print("key 값 continued의 value는 ",testHashTable.get_value('continued'))
+print("key 값 success의 value는 ",testHashTable.get_value('success'))
+print("key 값 unnoticed의 value는 ",testHashTable.get_value('unnoticed'))
         
 
