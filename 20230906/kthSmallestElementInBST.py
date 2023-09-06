025370147ee3942ca80cb24c589a162b8cd66212
wodnrP <wogajang22@naver.com>
2023-09-06 21:37:19 +0900

    refactor: Kth Smallest Element in BST 해결 코드 개선

--- 20230906/kthSmallestElementInBST.py
+++ 20230906/kthSmallestElementInBST.py
@@ -4,16 +4,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# 1차
+# class Solution:
+#     def kthSmallest(self, root, k):
+#         r = []
+#         self.inorder(root, r)
+#         r.sort()
+#         return r[k-1]
+
+#     def inorder(self,node,r):
+#         r.append(node.val)
+#         if node.left:
+#             self.inorder(node.left,r)
+#         if node.right:
+#             self.inorder(node.right,r)
+
+# 수정 후
 class Solution:
     def kthSmallest(self, root, k):
         r = []
-        self.inorder(root, r)
-        r.sort()
-        return r[k-1]
+        node = root
+        while True:
+            # 노드 왼쪽만 탐색, stack의 방법으로 노드 저장
+            while node is not None:
+                r.append(node)
+                node = node.left
 
-    def inorder(self,node,r):
-        r.append(node.val)
-        if node.left:
-            self.inorder(node.left,r)
-        if node.right:
-            self.inorder(node.right,r)
+            # r == None : root 노드 없음
+            if not r:
+                break
+
+            # 가장 최근에 push한 노드 반환
+            result = r.pop()
+            k -= 1
+
+            # k == 0 : k번째 push한 k번째 작은 값
+            if k == 0:
+                return result.val
+            
+            # 오른쪽 노드 탐색
+            node = result.right
