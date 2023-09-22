60c36a9590452a6f408f217a73eee5f363a777b9
wodnrP <wogajang22@naver.com>
2023-09-22 21:50:16 +0900

    refactor:  Remove Nth Node From End of List 개선 코드 추가

--- 202309/20230922/removeNthNodeFromEndofList.py
+++ 202309/20230922/removeNthNodeFromEndofList.py
@@ -2,23 +2,40 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+# 개선 전
+# class Solution:
+#     def removeNthFromEnd(self, head, n):
+#         dummy = []
+#         while head:
+#             dummy.append(head.val)
+#             head = head.next
+#         dummy.pop(-n)
+#         if not dummy:
+#             return 
+#         for i in range(len(dummy)):
+#             dummy[i] = ListNode(dummy[i])
+#         from collections import deque
+#         queue = deque(dummy)
+#         result = queue.popleft()
+#         curr = result
+#         while queue:
+#             node = queue.popleft()
+#             curr.next = node
+#             curr = node 
+#         return result
+
+# 개선 후 
 class Solution:
     def removeNthFromEnd(self, head, n):
-        dummy = []
-        while head:
-            dummy.append(head.val)
+        slow, fast = head, head
+        while n > 0:
+            fast = fast.next
+            n -= 1
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next
+        if fast:
+            slow.next = slow.next.next
+        else:
             head = head.next
-        dummy.pop(-n)
-        if not dummy:
-            return 
-        for i in range(len(dummy)):
-            dummy[i] = ListNode(dummy[i])
-        from collections import deque
-        queue = deque(dummy)
-        result = queue.popleft()
-        curr = result
-        while queue:
-            node = queue.popleft()
-            curr.next = node
-            curr = node 
-        return result
+        return head
