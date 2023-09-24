a868043dc6f27a90b4d16e2568b9fc09bb3b111c
wodnrP <wogajang22@naver.com>
2023-09-24 22:25:54 +0900

    refactor: Maximum Depth of Binary Tree 개선 코드 추가

--- 202309/20230924/maximumDepthofBinaryTree.py
+++ 202309/20230924/maximumDepthofBinaryTree.py
@@ -1,15 +1,23 @@
+# 수정 전
+# class Solution:
+#     def maxDepth(self, root):
+#         depth=0
+#         result=[]
+#         self.preorder(root, depth, result)
+#         result=sorted(set(result))
+#         return result[-1]
+#     def preorder(self, root, depth, result):
+#         if root is None:
+#             result.append(depth)
+#             pass
+#         else:
+#             depth+=1
+#             self.preorder(root.left, depth, result)
+#             self.preorder(root.right, depth, result)
+
+# 수정 후 
 class Solution:
     def maxDepth(self, root):
-        depth=0
-        result=[]
-        self.preorder(root, depth, result)
-        result=sorted(set(result))
-        return result[-1]
-    def preorder(self, root, depth, result):
         if root is None:
-            result.append(depth)
-            pass
-        else:
-            depth+=1
-            self.preorder(root.left, depth, result)
-            self.preorder(root.right, depth, result)
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
