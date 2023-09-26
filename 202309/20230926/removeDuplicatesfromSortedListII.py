81d23788fa8db4e375fa0bf24fb5b83b4e240b82
wodnrP <wogajang22@naver.com>
2023-09-26 22:17:19 +0900

    refactor: Remove Duplicates From Sorted List II 개선 코드 추가

--- 202309/20230926/removeDuplicatesfromSortedListII.py
+++ 202309/20230926/removeDuplicatesfromSortedListII.py
@@ -3,32 +3,51 @@ class ListNode:
         self.val = val
         self.next = next
 
+# 수정 전
+# class Solution:
+#     def deleteDuplicates(self, head):
+#         # num에 Linked List 데이터 담기
+#         num=[]
+#         while head:
+#             num.append(head.val)
+#             head = head.next
+        
+#         # num 값을 key, 중복 개수를 value로 하는 dict 
+#         num_dict={}
+#         for i in num:
+#             num_dict[i] = num_dict.get(i, 0) + 1
+        
+#         # value > 1인 key 삭제
+#         duplicate = [key for key, value in num_dict.items() if value > 1]
+#         for i in duplicate:
+#             del num_dict[i]
+        
+#         # key 기준 다시 리스트로 변경, 링크드 리스트로 재생성
+#         num = [i for i in num_dict.keys()]
+#         result = ListNode()
+#         curr = result
+#         i=0
+#         while len(num) > i:
+#             node = ListNode(num[i])
+#             curr.next = node
+#             curr = node
+#             i+=1
+#         return result.next
+    
+# 수정 후 
 class Solution:
     def deleteDuplicates(self, head):
-        # num에 Linked List 데이터 담기
-        num=[]
+        num_dict = {}
         while head:
-            num.append(head.val)
+            num_dict[head.val] = num_dict.get(head.val, 0) + 1
             head = head.next
-        
-        # num 값을 key, 중복 개수를 value로 하는 dict 
-        num_dict={}
-        for i in num:
-            num_dict[i] = num_dict.get(i, 0) + 1
-        
-        # value > 1인 key 삭제
         duplicate = [key for key, value in num_dict.items() if value > 1]
         for i in duplicate:
             del num_dict[i]
-        
-        # key 기준 다시 리스트로 변경, 링크드 리스트로 재생성
-        num = [i for i in num_dict.keys()]
         result = ListNode()
         curr = result
-        i=0
-        while len(num) > i:
-            node = ListNode(num[i])
+        for i in num_dict:
+            node = ListNode(i)
             curr.next = node
             curr = node
-            i+=1
         return result.next
