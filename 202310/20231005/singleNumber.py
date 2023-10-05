fc94992d874927dafacabbd7a2b25ae3490b50d3
wodnrP <wogajang22@naver.com>
2023-10-05 23:15:46 +0900

    chore: Single Number Solution 코드 추가

--- 202310/20231005/singleNumber.py
+++ 202310/20231005/singleNumber.py
@@ -8,3 +8,11 @@ class Solution:
             i+=2
             j=i+1
         return nums[i]
+
+# 솔루션 코드 추가 : 비트연산 
+# class Solution:
+#     def singleNumber(self, nums) -> int:
+#         num = 0
+#         for i in nums:
+#             num ^= i
+#         return num
