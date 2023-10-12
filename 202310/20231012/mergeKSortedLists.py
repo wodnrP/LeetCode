0528a121b94b51ccc1db7f32fe58fde376d33409
wodnrP <wogajang22@naver.com>
2023-10-12 21:51:27 +0900

    refactor: Merge K Sorted Lists 개선 코드 추가

--- 202310/20231012/mergeKSortedLists.py
+++ 202310/20231012/mergeKSortedLists.py
@@ -4,6 +4,27 @@ class ListNode:
         self.next = next
 
 # 1차        
+# class Solution:
+#     def mergeKLists(self, lists):
+#         link=[]
+#         i=0
+#         while len(lists)-1 >= i:
+#             if lists[i]:
+#                 link.append(lists[i].val)
+#             else:
+#                 i+=1
+#                 continue
+#             lists[i]=lists[i].next
+#         link=sorted(link)
+#         result=ListNode()
+#         curr=result
+#         for i in link:
+#             node=ListNode(i)
+#             curr.next=node
+#             curr=node
+#         return result.next
+    
+# 개선     
 class Solution:
     def mergeKLists(self, lists):
         link=[]
@@ -15,11 +36,8 @@ class Solution:
                 i+=1
                 continue
             lists[i]=lists[i].next
-        link=sorted(link)
-        result=ListNode()
-        curr=result
+        link=sorted(link, reverse=True)
+        result=None
         for i in link:
-            node=ListNode(i)
-            curr.next=node
-            curr=node
-        return result.next
+            result=ListNode(i, result)
+        return result
